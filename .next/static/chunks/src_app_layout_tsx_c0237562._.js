d425d7dbb75bdc1dd2d88cf1b8bc5596ec0c089f
Ron Egli <ron@r-egli.com>
2025-06-12 14:15:36 -0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749762935872

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_b3e0fd7c._.js",
+  "static/chunks/src_b2e4b9c3._.js"
+],
+    source: "dynamic"
+});
