e239f69c9f0e5932dc801ccdc224fb2d317b23c8
Ron Egli <ron@r-egli.com>
2025-06-12 14:15:05 -0700

    Add .next/static/chunks/src_app_console_page_tsx_b94f174c._.js 1749762904368

---
+++ .next/static/chunks/src_app_console_page_tsx_b94f174c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_console_page_tsx_d4b84a14._.js"
+],
+    source: "dynamic"
+});
